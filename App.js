f43800eafb860bdacdf8eb7f54e2b2d024370cc9
jonnahfreire <jonnas.freire17@gmail.com>
2021-02-01 20:55:59 -0300

    finished

--- App.js
+++ App.js
@@ -1,21 +1,163 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { 
+  TouchableOpacity,
+  StyleSheet, 
+  Text, 
+  View, 
+  KeyboardAvoidingView, 
+  Image, 
+  TextInput,
+  Animated,
+  Keyboard
+} from 'react-native';
 
 export default function App() {
+
+  const [offset] = useState(new Animated.ValueXY({x: 0, y: 80}));
+  const [opacity] = useState(new Animated.Value(0))
+  const [logo] = useState(new Animated.ValueXY({x:130, y:155}))
+
+  function keyboardDidShow() {
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 55,
+        duration: 100,
+        useNativeDriver: false,
+      }),
+      Animated.timing(logo.y, {
+        toValue: 65,
+        duration: 100,
+        useNativeDriver: false,
+      })
+    ]).start();
+  }
+
+  function keyboardDidHide() {
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 130,
+        duration: 100,
+        useNativeDriver: false,
+      }),
+      Animated.timing(logo.y, {
+        toValue: 155,
+        duration: 100,
+        useNativeDriver: false,
+      })
+    ]).start();
+  }
+
+  useEffect(() => {
+    keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', keyboardDidShow);
+    keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', keyboardDidHide);
+
+    Animated.parallel([
+      Animated.spring(offset.y, {
+        toValue: 0,
+        speed: 4,
+        bounciness: 20,
+        useNativeDriver: true,
+      }),
+      Animated.timing(opacity, {
+        toValue:1,
+        duration: 200,
+        useNativeDriver: true,
+      })
+    ]).start();
+
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.containerLogo}>
+        <Animated.Image
+        style={{
+          width: logo.x,
+          height: logo.y
+        }}
+          source={require('./src/assets/logo.png')}
+        />
+      </View>
+      <Animated.View 
+          style={[styles.container,
+          {
+            opacity: opacity,
+            transform: [
+              {translateY: offset.y}
+            ]
+          }
+          ]}
+        >
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          secureTextEntry={true}
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.submitText}>Acessar</Text>
+        </TouchableOpacity>
+      
+        <TouchableOpacity style={styles.btnRegister}>
+          <Text style={styles.registerText}>Criar conta gratuita</Text>
+        </TouchableOpacity>
+
+      </Animated.View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    paddingBottom: 50
+  },
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#191919',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerLogo: {
+    flex:1,
+    justifyContent:'center'
+  },
+  input:{
+    backgroundColor:'#FFF',
+    width: '90%',
+    height: 45,
+    padding: 10,
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7
+  },
+  btnSubmit: {
+    backgroundColor: '#35AAFF',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+  },
+  submitText:{
+    color: '#FFF',
+    fontSize: 18
+  },
+  btnRegister:{
+    marginTop: 10,
+  },
+  registerText:{
+    color: '#FFF'
+  }
 });
